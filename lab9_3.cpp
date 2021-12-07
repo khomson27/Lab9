8590ad8b5521f03d5dc41a2e6a4ca5855d00e7b7
bukbunny <khomsan_su@cmu.ac.th>
2021-12-07 10:10:48 +0700

    complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,51 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    int z;
+    string T;
+    cout << "Enter your age: ";
+    cin >> x;
+    if (x <= 20)
+        {
+        cout << "Enter your height: ";
+        cin >> y;
+        if (y < 160)
+            {
+                T = "UNFRIEND";
+            } else if(y < 175)
+            {
+                T = "FRIEND";
+            } else
+            {
+              cout << "Enter your property: ";
+              cin >> z;
+              if (z > 69000000)
+              {
+                T = "MARRIED";
+              } else 
+              {
+                T = "ONE-NIGHT-STAND";
+              }  
+            } 
+        } else if(x < 30)
+        {
+            cout << "Enter your property: ";
+            cin >> z;
+            if (z > 10000000)
+            {
+                T = "BEST FRIEND";
+            } else
+            {
+                T = "UNFRIEND";
+            }
+
+        } else 
+        {
+            T = "UNFRIEND";
+        }
+ cout << "Your status = " << T;
+ return 0;
+}
